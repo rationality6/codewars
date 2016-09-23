c3e316388bf3a1f6c0a2baa3860f79b24ae38fc2
Hyun Ahn <rationality6@gmail.com>
2016-09-23 15:56:02 -0700

    add practice codes

--- 6kyu/checking_groups.rb
+++ 6kyu/checking_groups.rb
@@ -1,22 +1,42 @@
+# def group_check(s)
+#
+#     hash_symbols = {
+#         '(':')',
+#         '{':'}',
+#         '[':']'
+#     }
+#
+#     splited_array = s.split("")
+#     stack = []
+#     for i in splited_array
+#         if i == '('
+#             stack << hash_symbols[:'(']
+#         end
+#     end
+#     print stack
+# end
+
+
 def group_check(s)
-
-    hash_symbols = {
-        '(':')',
-        '{':'}',
-        '[':']'
-    }
-
-    splited_array = s.split("")
-    stack = []
-    for i in splited_array
-        if i == '('
-            stack << hash_symbols[:'(']
-        end
-    end
-    print stack
+  stack = []
+  symbols = { '{' => '}', '[' => ']', '(' => ')' }
+  s.each_char do |c|
+    stack << c if symbols.key?(c)
+    return false if symbols.key(c) && symbols.key(c) != stack.pop
+  end
+  stack.empty?
 end
 
-group_check('({})')
 
-rr = [1,'jan'] => "January"
-p rr
+p group_check('({})')
+p group_check('({})}')
+p group_check('({)}')
+
+'({})'.each_char do |c|
+
+end
+
+symbols = { '{' => '}', '[' => ']', '(' => ')' }
+if symbols.key('{')
+  p 'exist'
+end
--- 7kyu/shortestWord.js
+++ 7kyu/shortestWord.js
@@ -13,12 +13,20 @@ function findShort(s){
   return Math.min.apply(null, s.split(' ').map(w => w.length));
 }
 
+function findLong(string){
+  return Math.max.apply(null,string.split(' ').map(x=>x.length));
+}
 
-// var a = [1,2,3,4,5].map(w=>w*2)
-// console.log(a);
-//
-// var a = Math.min(null,[9,9,3,4,5])
-// console.log(a);
+
+var a = [1,2,3,4,5].map(w=>w*2)
+console.log(a);
+
+var a = Math.min.apply(null,[9,9,3,4,5])
+console.log(a);
 
 console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
+console.log(findShort("n take over the world maybe who knows perhaps"));
+console.log(findLong("bitcoin take over the world maybe who knows perhaps"));
+console.log(findLong("bitcoinwoefijoweij take over the world maybe who knows perhaps"));
 console.log(findShort("turns out random test cases are easier than writing out basic ones"));
+console.log(findLong("turns out random test cases are easier than writing out basic ones"));
