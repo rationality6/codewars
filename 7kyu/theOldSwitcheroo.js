049ff0d3508d838fae4e913262de11b8d28236b8
rationality6 <rationality6@gmail.com>
2016-09-30 11:23:34 -0700

    bunch of practices

---
+++ 7kyu/theOldSwitcheroo.js
@@ -0,0 +1,25 @@
+// const vowel2index = (str) => {
+//   let splited = str.split('')
+//   let array = ""
+//   splited.map((value, index, self) => {
+//     if (value === 'i' || value === 'e' || value === 'o' || value === 'a' || value === 'U' || value === 'I' || value === 'E' || value === 'O' || value === 'A' || value === 'U') {
+//       array += index + 1
+//     } else {
+//       array += value
+//     }
+//   })
+//   return array
+// }
+//
+
+// const vowel2index = (str) => str.replace(/[aeiou]/ig,(m,i)=>i+1)
+const vowel2index = (str) => str.replace(/[aeiou]/ig,(m,i)=>i+1)
+
+console.log(vowel2index('this is my string'));
+console.log(vowel2index('Codewars is the best site in the world'));
+console.log(vowel2index('Tomorrow is going to be raining'));
+console.log(vowel2index(''));
+
+// vowel2index('Codewars is the best site in the world'), 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld');
+// vowel2index('Tomorrow is going to be raining'), 'T2m4rr7w 10s g1415ng t20 b23 r2627n29ng');
+// vowel2index(''), '');
---
+++ practices/letPrac.js
@@ -0,0 +1,27 @@
+const varTest = () => {
+  var x = 31;
+  if (true) {
+    var x = 71;  // same variable!
+    console.log(x);  // 71
+  }
+  console.log(x);  // 71
+}
+
+varTest()
+
+const letTest = () => {
+  let x = 31;
+  if (true) {
+    let x = 71;  // different variable
+    console.log(x);  // 71
+  }
+  console.log(x);  // 31
+}
+
+letTest()
+
+
+console.log(i);
+for(var i=0;i<3;i++){
+
+}
