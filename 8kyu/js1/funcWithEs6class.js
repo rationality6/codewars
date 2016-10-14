12f23ddc3fc5539555c923b30f3cb30c72534a41
rationality6 <rationality6@gmail.com>
2016-10-14 10:21:18 -0700

    pratices

---
+++ 8kyu/js1/funcWithEs6class.js
@@ -0,0 +1,30 @@
+// class Person {
+//   constructor(firstName = 'John', lastName = 'Doe', age = 0, gender = 'Male') {
+//     Object.assign(this, { firstName, lastName, age, gender });
+//   }
+//   sayFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+//   static greetExtraTerrestrials(raceName) {
+//     return `Welcome to Planet Earth ${raceName}`;
+//   }
+// }
+
+
+class Person {
+  constructor(firstName = "John", lastName = 'Doe', age = 0, gender = 'Male') {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender;
+  }
+  sayFullName(){
+    return `${this.firstName} ${this.lastName}`
+  }
+  static greetExtraTerrestrials(raceName){
+    return `Welcome to Planet Earth ${raceName}`
+  }
+}
+
+paul = new Person('f')
+console.log(paul.sayFullName());
---
+++ practices/js1/findDuplicationEs6.js
@@ -0,0 +1,12 @@
+import qunit = 'qunit'
+
+const arr0 = [12, 9, 9, 111, 2, 3, 4, 4, 5, 7, 12];
+
+const findDuplication = arr => {
+  return [...arr].sort((a, b) => a - b).filter((elem, index, arr) => {
+    return arr[index + 1] === elem
+  })
+}
+
+console.log(findDuplication(arr0));
+// [ 4, 9, 12]
