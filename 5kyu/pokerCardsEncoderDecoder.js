235cd31552370b99d7f74e5d24ed1f11280e3dff
rationality6 <rationality6@gmail.com>
2016-10-31 21:15:56 -0700

    pokercardsencoderdecoder middle save

---
+++ 5kyu/pokerCardsEncoderDecoder.js
@@ -0,0 +1,29 @@
+let cardClass = ['c', 'd', 'h', 's']
+let cardLevel = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 'T', 'J', 'Q', 'K']
+
+const cardTableMaker = () => {
+  return cardClass.map(a=>{
+    return cardLevel.map(b=>{
+      return `${b}${a}`
+    })
+  })
+}
+
+const flatten = n => [].concat.apply([],n)
+
+const cardTable = flatten(cardTableMaker())
+
+const cardsConverter = input => {
+  input.map(a=>{
+    cardTable.map(b=>{
+      console.log(a);
+      // console.log(b);
+    })
+  })
+}
+
+console.log(cardsConverter([50, 6, 13,30, 37]));
+// , ["7c", "Ad", "5h", "Qh", "Qs"], 'should return ["7c", "Ad", "5h", "Qh", "Qs"]');
+
+// console.log(cardsConverter(["5h", "7c", "Qh", "Qs", "Ad"]));
+// , [6, 13, 30, 37, 50], 'should return [6, 13, 30, 37, 50]');
